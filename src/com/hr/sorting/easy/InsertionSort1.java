7bfc8521e766433d5a7c62becf5a047237caf17f
Ankur <ankur93jain@gmail.com>
2018-02-22 16:36:13 +0530

    sorting questions

---
+++ src/com/hr/sorting/easy/InsertionSort1.java
@@ -0,0 +1,48 @@
+package com.hr.sorting.easy;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class InsertionSort1 {
+
+    static void insertionSort1(int n, int[] arr) {
+        // Complete this function
+    	for (int i = 1; i < n; i++) {
+
+			int value = arr[i];
+			int index = i;
+			
+			while(index > 0 && arr[index - 1] > value){
+				arr[index] = arr[index - 1];
+					index--;
+					print(arr, n);
+					System.out.println();
+			}
+			
+			arr[index] = value; 
+			
+		}
+    	
+    	print(arr, n);
+
+    }
+    
+    private static void print(int arr[], int n){
+    	for(int i=0; i<n;i++){
+    		System.out.print(arr[i] + " ");
+    	}
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        insertionSort1(n, arr);
+        in.close();
+    }
+}
---
+++ src/com/hr/sorting/easy/Quicksort1Partition.java
@@ -0,0 +1,62 @@
+package com.hr.sorting.easy;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Quicksort1Partition {
+
+    static int[] quickSort(int[] arr) {
+        // Complete this function
+    	
+		int pivot = arr[0];
+		List<Integer> leftList = new ArrayList<>();
+		List<Integer> rightList = new ArrayList<>();
+		int j=0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if(arr[i] < pivot)
+				leftList.add(arr[i]);
+			else if(arr[i] > pivot)
+				rightList.add(arr[i]);
+			
+		}
+		
+		for (int i = 0; i < leftList.size(); i++) {
+			
+			arr[j] = leftList.get(i);
+			j++;
+		}
+		
+		arr[j] = pivot;
+		j++;
+		
+		for (int i = 0; i < rightList.size(); i++) {
+			
+			arr[j] = rightList.get(i);
+			j++;
+		}
+		
+		return arr;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        int[] result = quickSort(arr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
